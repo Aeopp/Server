9ed3d3a64eb449333f3af095882662f3424f9f58
Aeopp <irmxj777@gmail.com>
2023-01-08 04:33:55 +0900

    spin lock, compare and swap

---
+++ GameServer/ServerCore/CorePch.h
@@ -0,0 +1,3 @@
+#pragma once
+// TODO :: ServerCore 프로젝트 이외에서 참조할 것들 여기에 정의한다.
+#include "Types.h"
---
+++ GameServer/ServerCore/Types.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#pragma region Standard Include...
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <atomic>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+using namespace chrono_literals;
+#pragma endregion Standard Include...
+
+#pragma region Built-in
+using int32 = __int32;
+using uint32 = unsigned __int32;
+using int64 = __int64;
+using uint64 = unsigned __int64;
+#pragma endregion Built-in
+
+#pragma region User-Custom
+using TimeUnit = milliseconds;
+#pragma endregion USer-Custom
